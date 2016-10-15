b84f6d37dd00dbe7ad504b5bf80493b2bfdddb93
Jihong Ha <lazy.jihong@gmail.com>
2016-10-15 19:02:22 +0900

    XFER TESTED

--- Central/Central.ino
+++ Central/Central.ino
@@ -14,23 +14,23 @@ WiFiManager wifiManager;
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-typedef struct {
-  int temperature;
-  int pressure;
-  int battery;
-} tpms_t;
+void send_cmd(byte id, byte *data, byte len);
+void parse_xfer_buf();
 
-tpms_t tpms[8];
-int door;
-int cargo[3];
-int LOFF;
-//test data 고정길이 and TEXT BASE
-// READ FORMAT
-// &IDX|DATATYPE|DATA|CRC|
-// &10|1|CRC|
-// &11|0|CRC|
-// TYPE 0 : CARGO + DOOR, TYPE 1 : TPMS
-// DATA TYPE : 0: CARGO 1:DOOR, 2: TEMPERATURE, 3:PRESSURE, 4:BATTERY
+#define XFER_CMD_BLE_SCAN   0
+#define XFER_CMD_BLE_SCAN_STOP   1
+#define XFER_CMD_BLE_ADD_CLIENT  2
+#define XFER_CMD_BLE_REMOVE_CLIENT  3
+#define XFER_EVENT_BLE_SCAN_DATA  4
+#define XFER_EVENT_SENSOR_DATA  5
+
+#define XFER_EVENT_ACK 128
+#define XFER_EVENT_NACK 129
+
+#define XFER_HEADER_SIZE 5 //AA,55,version,id,len
+
+byte xfer_buf[512];
+int xfer_size;
 
 static const char PROGMEM INDEX_HTML[] = R"rawliteral(
 <!DOCTYPE html>
@@ -116,11 +116,6 @@ function buttonclick(e) {
 </html>
 )rawliteral";
 
-// GPIO#0 is for Adafruit ESP8266 HUZZAH board. Your board LED might be on 13.
-const int LEDPIN = 0;
-// Current LED status
-bool LEDStatus;
-
 // Commands sent through Web Socket
 const char BLESCAN[] = "BLESCAN";
 const char BLESTOP[] = "BLESTOP";
@@ -146,18 +141,22 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       if (strcmp(BLESCAN, (const char *)payload) == 0) {
         //Send BLE SCAN CMD to Meag
         Serial.printf("%s\n", payload);
+        send_cmd(XFER_CMD_BLE_SCAN, NULL, 0);
       }
       else if (strcmp(BLESTOP, (const char *)payload) == 0) {
         //Send BLE SCAN STOP CMD to Mega
         Serial.printf("%s\n", payload);
+        send_cmd(XFER_CMD_BLE_SCAN_STOP, NULL, 0);
       }
       else if (strncmp(BLEADD, (const char *)payload, 6) == 0) {  //FIXME:strncmp
-        //Send BLE ADD CMD to Mega 
+        //Send BLE ADD CMD to Mega
         Serial.printf("%s\n",payload);
+        send_cmd(XFER_CMD_BLE_ADD_CLIENT, NULL, 0); //FIXME
       }
       else if (strncmp(BLEREMOVE, (const char *)payload, 9) == 0) { //FIXME:strncmp
-        //Send BLE ADD CMD to Mega 
+        //Send BLE ADD CMD to Mega
         Serial.printf("%s\n",payload);
+        send_cmd(XFER_CMD_BLE_REMOVE_CLIENT, NULL, 0);
       }
       else {
         Serial.println("Unknown command");
@@ -178,7 +177,8 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
   }
 }
 
-void handleRoot() {
+void handleRoot()
+{
   server.send(200, "text/html", INDEX_HTML);
 }
 
@@ -198,39 +198,40 @@ void handleNotFound()
   server.send(404, "text/plain", message);
 }
 
-void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    //WiFiManager wifiManager;
-    //reset saved settings
-    //wifiManager.resetSettings();
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  //WiFiManager wifiManager;
+  //reset saved settings
+  //wifiManager.resetSettings();
 
-    //set custom ip for portal
-    wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  //set custom ip for portal
+  wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    String ChipId = "ConAP_";
-    ChipId += String(ESP.getChipId(), HEX);
-    ChipId.toUpperCase();
-    Serial.println(ChipId);
-    char buf[32] = "";
-    ChipId.toCharArray(buf, 32);
-    wifiManager.autoConnect(buf, "12345678");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-
-
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  String ChipId = "ConAP_";
+  ChipId += String(ESP.getChipId(), HEX);
+  ChipId.toUpperCase();
+  Serial.println(ChipId);
+  char buf[32] = "";
+  ChipId.toCharArray(buf, 32);
+  wifiManager.autoConnect(buf, "12345678");
+  //or use this for auto generated name ESP + ChipID
+  //wifiManager.autoConnect();
 
 
-    if (mdns.begin("apcon", WiFi.localIP())) {
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+
+  if (mdns.begin("apcon", WiFi.localIP())) {
     Serial.println("MDNS responder started");
     mdns.addService("http", "tcp", 80);
     mdns.addService("ws", "tcp", 81);
@@ -241,7 +242,7 @@ void setup() {
     server.begin();
 
     webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
+    webSocket.onEvent(webSocketEvent);
     Serial.println("Server started");
 
   }
@@ -253,7 +254,91 @@ void setup() {
 
 }
 
-void loop() {
+void send_cmd(byte id, byte *data, byte len)
+{
+    int i;
+
+    Serial.write(0xAA);
+    Serial.write(0x55);
+    Serial.write(0x01);
+    Serial.write(id);
+    Serial.write(len);
+    for(i=0;i<len;i++)
+      Serial.write(data[i]);
+
+    Serial.println("Send CMD Done.");
+}
+void parse_xfer_buf()
+{
+  byte *buf = xfer_buf;
+  byte id = 0, version = 0, len = 0;
+  int size = xfer_size;
+  int wait_data = 0;
+
+  while(size >= XFER_HEADER_SIZE) {
+    if(*buf++ != 0xAA) {
+      size--;
+      continue;
+    }
+
+    if(*buf++ != 0x55) {
+      size--;
+      continue;
+    }
+
+    version = *buf++;
+    size--;
+
+    id = *buf++;
+    size--;
+
+    len = *buf++;
+    size--;
+
+    if(size < len) { //wait more data
+      int i;
+
+      xfer_buf[0] = 0xAA;
+      xfer_buf[1] = 0x55;
+      xfer_buf[2] = version;
+      xfer_buf[3] = id;
+      xfer_buf[4] = len;
+
+      for(i=0;i<size;i++) {
+          xfer_buf[5+i] = buf[i];
+      }
+
+      xfer_size = size + 5;
+      wait_data = 1;
+      break;
+    }
+
+    //Serial.print(id);
+    //Serial.println("data Received");
+    //parse data
+    switch (id) {
+      case XFER_EVENT_BLE_SCAN_DATA:
+      //webSocket.broadcastTXT(LEDON, strlen(LEDON));
+      send_cmd(XFER_EVENT_ACK, NULL, 0);
+      break;
+      case XFER_EVENT_SENSOR_DATA:
+      send_cmd(XFER_EVENT_NACK, NULL, 0);
+      break;
+    }
+    size -= len;
+    buf +=len;
+  }
+
+  if(size && wait_data == 0) {
+    int i;
+    for(i=0;i<size;i++)
+    xfer_buf[i] = buf[i];
+    xfer_size = size;
+  }
+}
+
+void loop()
+{
     // put your main code here, to run repeatedly:
     //check gpio and reset settings
     if(digitalRead(2) == 0) {
@@ -266,6 +351,7 @@ void loop() {
       ESP.restart();
     }
 
+#if 0
     //TODO : read data to uart (BLE Data)
     if(Serial.available() > 0) {
         String eventString;
@@ -274,19 +360,20 @@ void loop() {
         eventString.toCharArray(eventChar,128);
         Serial.printf("[%s]\n", eventChar);
     }
+#endif
+    while(Serial.available() > 0) {
+      if(xfer_size < 512) {
+        xfer_buf[xfer_size] = Serial.read();
+        xfer_size++;
+      } else {
+          //xfer_buf full : flush?
+          parse_xfer_buf();
+      }
+    }
+    parse_xfer_buf();
 
     //TODO : add web server setup page for add client and server ip
     webSocket.loop();
     server.handleClient();
 
-#if 0
-    if(LOFF ==1) {
-    webSocket.broadcastTXT(LEDOFF, strlen(LEDOFF));
-    LOFF = 0;
-    } else {
-      webSocket.broadcastTXT(LEDON, strlen(LEDON));
-    LOFF = 1;
-    }
-    delay(1000);
-#endif
 }
--- MegaServer/MegaServer.ino
+++ MegaServer/MegaServer.ino
@@ -1,109 +1,247 @@
-/*
-  SerialPassthrough sketch
-
-  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro,
-  have one hardware serial port attached to Digital pins 0-1, and a
-  separate USB serial port attached to the IDE Serial Monitor.
-  This means that the "serial passthrough" which is possible with
-  the Arduino UNO (commonly used to interact with devices/shields that
-  require configuration via serial AT commands) will not work by default.
-
-  This sketch allows you to  emulate the serial passthrough behaviour.
-  Any text you type in the IDE Serial monitor will be written
-  out to the serial port on Digital pins 0 and 1, and vice-versa.
-
-  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
-  attached to the Serial Monitor, and "Serial1" refers to the hardware
-  serial port attached to pins 0 and 1. This sketch will emulate Serial passthrough
-  using those two Serial ports on the boards mentioned above,
-  but you can change these names to connect any two serial ports on a board
-  that has multiple ports.
-
-   Created 23 May 2016
-   by Erik Nyquist
-*/
 #include <EEPROM.h>
+
+#define XFER_HEADER_SIZE 5 //AA,55,version,id,len
+
+#define XFER_CMD_BLE_SCAN   0
+#define XFER_CMD_BLE_SCAN_STOP   1
+#define XFER_CMD_BLE_ADD_CLIENT  2
+#define XFER_CMD_BLE_REMOVE_CLIENT  3
+#define XFER_EVENT_BLE_SCAN_DATA  4
+#define XFER_EVENT_SENSOR_DATA  5
+
+#define XFER_EVENT_ACK 128
+#define XFER_EVENT_NACK 129
+
+byte xfer_buf[512];
+int xfer_size = 0;
+byte xfer_buf2[512];
+int xfer_size2 = 0;
+
+byte BLEClient[16][7];
 
 void BLEClientLoad(int idx);  //0~15
 void BLEClientSave(int idx);
 
-void BL620EventLoop();
+void BLEEventLoop();
 void ESP8266EventLoop();
+void UserEventLoop();
 
-byte BLEClient[16][7];
-void setup() {
-  int i;
-  
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  Serial2.begin(9600);
+void parse_xfer_buf();
+void parse_xfer_buf2();
 
-#if 0
-  BLEClient[0][0] = 0x00;
-  BLEClient[0][1] = 0x00;
-  BLEClient[0][2] = 0x00;
-  BLEClient[0][3] = 0xAA;
-  BLEClient[0][4] = 0xAB;
-  BLEClient[0][5] = 0x05;
-  BLEClient[0][6] = 0xAD;
-  BLEClientSave(0);
-  BLEClient[0][0] = 0x00;
-  BLEClient[0][1] = 0x00;
-  BLEClient[0][2] = 0x00;
-  BLEClient[0][3] = 0x00;
-  BLEClient[0][4] = 0x00;
-  BLEClient[0][5] = 0x00;
-  BLEClient[0][6] = 0x00;
-  BLEClientLoad(0);
-  for(i=0;i<7;i++) {
-  if(BLEClient[0][i] < 0x10)
-    Serial.print('0');
-  Serial.print(BLEClient[0][i], HEX);
-  }
-  Serial.println("");
-  #endif
-}
+void send_data(byte cmd, byte *data, byte len);
 
-void loop() {
-  //BL620EventLoop();
-
-  ESP8266EventLoop();
-
-  #if 0     //FOR TEST ONLY
-  if (Serial.available()) {      // If anything comes in Serial (USB),
-    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-  }
-  #endif
-
-  //TEST
-    Serial2.println("BLESCANDATA:00AABBCCDDEEFF|AAAAAAAAAAAA|AAAAAAAAAAAAAAA|AAAAAAAAAAAAA|AAAAAAAAAAAA");
-    delay(1000);
-}
-
-void BL620EventLoop()
+void send_data(byte cmd, byte *data, byte len)
 {
-  if (Serial1.available()) {    
-    Serial.println("");
-    Serial.print("BL620: ");
-    Serial.print(Serial1.readStringUntil('\n'));   // read it and send it out Serial (USB)
-    Serial.println("");
-  }  
+    int i;
+
+    Serial2.write(0xAA);
+    Serial2.write(0x55);
+    Serial2.write(0x01);
+    Serial2.write(cmd);
+    Serial2.write(len);
+    for(i=0;i<len;i++)
+      Serial2.write(data[i]);
+
+    Serial.println("Send Data Done.");
+}
+
+void parse_xfer_buf()
+{
+  byte *buf = xfer_buf;
+  byte id = 0, version = 0, len = 0;
+  int size = xfer_size;
+  int wait_data = 0;
+
+  while(size >= XFER_HEADER_SIZE) {
+    if(*buf++ != 0xAA) {
+      size--;
+      continue;
+    }
+
+    if(*buf++ != 0x55) {
+      size--;
+      continue;
+    }
+
+    version = *buf++;
+    size--;
+
+    id = *buf++;
+    size--;
+
+    len = *buf++;
+    size--;
+
+    if(size < len) { //wait more data
+      int i;
+
+      xfer_buf[0] = 0xAA;
+      xfer_buf[1] = 0x55;
+      xfer_buf[2] = version;
+      xfer_buf[3] = id;
+      xfer_buf[4] = len;
+
+      for(i=0;i<size;i++) {
+          xfer_buf[5+i] = buf[i];
+      }
+
+      xfer_size = size + 5;
+      wait_data = 1;
+      break;
+    }
+
+    Serial.print(id);
+    Serial.println(" Received");
+
+    switch (id) {
+      case XFER_CMD_BLE_SCAN:
+        //test
+        send_data(XFER_EVENT_BLE_SCAN_DATA, NULL, 0);
+      break;
+      case XFER_CMD_BLE_SCAN_STOP:
+      break;
+      case XFER_CMD_BLE_ADD_CLIENT:
+      break;
+      case XFER_CMD_BLE_REMOVE_CLIENT:
+      break;
+      case XFER_EVENT_ACK:
+        Serial.println("ACK Received");
+      break;
+      case XFER_EVENT_NACK:
+        Serial.println("NACK Received");
+      break;
+    }
+
+    size -= len;
+    buf +=len;
+  }
+
+  if(size && wait_data == 0) {
+    int i;
+    for(i=0;i<size;i++)
+    xfer_buf[i] = buf[i];
+    xfer_size = size;
+  }
+}
+
+void parse_xfer_buf2()
+{
+  byte *buf = xfer_buf2;
+  byte id = 0, version = 0, len = 0;
+  int size = xfer_size2;
+  int wait_data = 0;
+
+  while(size >= XFER_HEADER_SIZE) {
+    if(*buf++ != 0xAA) {
+      size--;
+      continue;
+    }
+
+    if(*buf++ != 0x55) {
+      size--;
+      continue;
+    }
+
+    version = *buf++;
+    size--;
+
+    id = *buf++;
+    size--;
+
+    len = *buf++;
+    size--;
+
+    if(size < len) { //wait more data
+      int i;
+
+      xfer_buf2[0] = 0xAA;
+      xfer_buf2[1] = 0x55;
+      xfer_buf2[2] = version;
+      xfer_buf2[3] = id;
+      xfer_buf2[4] = len;
+
+      for(i=0;i<size;i++) {
+          xfer_buf2[5+i] = buf[i];
+      }
+
+      xfer_size2 = size + 5;
+      wait_data = 1;
+      break;
+    }
+
+    Serial.print(id);
+    Serial.println("CMD Received");
+    //parse data
+    switch (id) {
+      case XFER_EVENT_BLE_SCAN_DATA:
+      break;
+      case XFER_EVENT_ACK:
+        Serial.println("ACK Received");
+      break;
+      case XFER_EVENT_NACK:
+        Serial.println("NACK Received");
+      break;
+    }
+    size -= len;
+    buf +=len;
+  }
+
+  if(size && wait_data == 0) {
+    int i;
+    for(i=0;i<size;i++)
+    xfer_buf2[i] = buf[i];
+    xfer_size2 = size;
+  }
+}
+
+void BLEEventLoop()
+{
+  while (Serial1.available() > 0) {
+    if(xfer_size2 <= 512) {
+      xfer_buf2[xfer_size2] = Serial1.read();
+      xfer_size2++;
+    } else {
+      parse_xfer_buf2();
+    }
+  }
+  parse_xfer_buf2();
+}
+
+//FIXME : Echo etc.
+void UserEventLoop()
+{
+  while (Serial.available() > 0) {
+    if(xfer_size <= 512) {
+      xfer_buf[xfer_size] = Serial.read();
+      xfer_size++;
+    } else {
+      //xfer_buf full : flush?
+      parse_xfer_buf();
+    }
+  }
+  //Serial.println("");
+  //Serial.print(xfer_size, HEX);
+  //Serial.println("");
 }
 
 void ESP8266EventLoop()
 {
-  if (Serial2.available()) {    
-    Serial.println("");
-    Serial.print("ESP8266: ");
-    Serial.print(Serial2.readStringUntil('\n'));   // read it and send it out Serial (USB)
-    Serial.println("");
+  while (Serial2.available() > 0) {
+    if(xfer_size <= 512) {
+      xfer_buf[xfer_size] = Serial2.read();
+      xfer_size++;
+    } else {
+      parse_xfer_buf();
+    }
   }
+  parse_xfer_buf();
 }
 
 void BLEClientLoad(int idx)  //0~15
 {
   int i;
-  
+
   for(i=0;i<7;i++)
     BLEClient[idx][i] = EEPROM.read(idx*7+i);
 
@@ -113,10 +251,27 @@ void BLEClientLoad(int idx)  //0~15
 void BLEClientSave(int idx)
 {
   int i;
-  
+
   for(i=0;i<7;i++)
     EEPROM.write(idx*7+i,BLEClient[idx][i]);
 
   Serial.println("BLEClientSave");
 }
 
+void setup()
+{
+  Serial.begin(9600);  //DEBUG
+  Serial1.begin(9600); //BLE
+  Serial2.begin(9600); //ESP8266
+}
+
+void loop()
+{
+  BLEEventLoop();
+  ESP8266EventLoop();
+
+//#define USER_EVENT
+#ifdef USER_EVENT
+  UserEventLoop();
+#endif
+}
